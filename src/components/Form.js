af903d1e3aefa0d0a41a5813e6d3fd3f58f3477e
Leo Kontogiorgis <leokon95@gmail.com>
2020-01-28 22:38:57 +1100

    Add form tooltips

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,7 @@
 import React from "react";
+import ReactTooltip from "react-tooltip";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faQuestionCircle} from "@fortawesome/free-solid-svg-icons";
 
 class Form extends React.Component {
     constructor(props) {
@@ -33,15 +36,26 @@ class Form extends React.Component {
                 <div className="form-header">Generate a QR code</div>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="ssid" placeholder="Network SSID" value={this.state.ssid} onChange={this.handleChange} />
-                    <input type="password" name="password" placeholder="Network Password" value={this.state.password} onChange={this.handleChange} />
-                    <select name="encryption" value={this.state.encryption} onChange={this.handleChange}>
-                        <option value="WPA/WPA2">WPA/WPA2</option>
-                        <option value="WEP">WEP</option>
-                        <option value="None">None</option>
-                    </select>
+                    <div className="input-wrapper">
+                        <input type="text" name="ssid" placeholder="Network SSID" value={this.state.ssid} onChange={this.handleChange} />
+                        <FontAwesomeIcon icon={faQuestionCircle} data-tip="The name you use to connect to your wifi network." />
+                    </div>
+                    <div className="input-wrapper">
+                        <input type="password" name="password" placeholder="Network Password" value={this.state.password} onChange={this.handleChange} />
+                        <FontAwesomeIcon icon={faQuestionCircle} data-tip="The password you use to connect to your wifi network." />
+                    </div>
+                    <div className="input-wrapper">
+                        <select name="encryption" value={this.state.encryption} onChange={this.handleChange}>
+                            <option value="WPA/WPA2">WPA/WPA2</option>
+                            <option value="WEP">WEP</option>
+                            <option value="None">None</option>
+                        </select>
+                        <FontAwesomeIcon icon={faQuestionCircle} data-tip="The type of encryption your wifi network uses. <br /> If you're not sure, leave the default!" />
+                    </div>
                     <input className="submit" type="submit" value="Generate" />
                 </form>
+
+                <ReactTooltip place="right" effect="solid" multiline={true} />
             </div>
         );
     }
